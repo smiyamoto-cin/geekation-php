42e75e88ed17817417dc42a3a737b84e4e45450a
宮本早織 <miyamotosaori@miyamotosaorinoMacBook-Pro.local>
2023-04-07 11:02:52 +0900

    first commit

--- lesson3.php
+++ lesson3.php
@@ -8,3 +8,12 @@
 // 20歳・女性
 
 // という形で出力してください。
+$profile = array(
+    'name' =>'山田',
+    'age' =>'20',
+    'gender'=>'女性'
+);
+
+echo "{$profile['name']}"."<br />";
+echo "{$profile['age']}歳・{$profile['gender']}"."<br />";
+?>
